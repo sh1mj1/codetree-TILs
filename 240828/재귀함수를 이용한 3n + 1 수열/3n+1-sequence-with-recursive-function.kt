53277912e1a2ac2be1edaee36382e0be7d29f489
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-08-28 16:34:07 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240828/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.kt
@@ -0,0 +1,23 @@
+val n = readLine().toInt()
+
+var count = 0
+
+fun f(num: Int): Int {
+    if(num == 1) {
+        return count
+    }
+
+    count++
+
+    if(num % 2 == 0) {
+        val quotient = num / 2   
+        return f(quotient)
+    }
+
+    val next = num * 3 + 1
+    return f(next)
+}
+
+fun main() {
+    println(f(n))
+}
