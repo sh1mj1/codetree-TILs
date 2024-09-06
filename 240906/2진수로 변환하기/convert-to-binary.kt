f28539139880bc6e6285a9a1647051810e37b00a
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-09-06 23:14:34 +0900

    Title: 2진수로 변환하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240906/2진수로 변환하기/convert-to-binary.kt
+++ 240906/2진수로 변환하기/convert-to-binary.kt
@@ -3,12 +3,16 @@ var n = readln().toInt()
 val remainders = mutableListOf<Int>()
 
 fun main() {
+    if(n == 0) {
+        println(0)
+        return
+    }
     while(n != 0) {
         val remainder = n % 2
         n = n / 2
-        remainders.add(remainder)
+        remainders.addLast(remainder)
    }
-   for(item in remainders.reversed()) {
+   for(item in remainders) {
         print(item)
    }
 }
