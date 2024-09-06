eb10426c193811f442bbd9d5c8c74f9cc32f5482
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-09-06 23:29:02 +0900

    Title: 10진수로 변환하기, Time: 98ms, Memory: 10MB, Status: Passed - CodeTree

--- 240906/10진수로 변환하기/convert-to-decimal.kt
+++ 240906/10진수로 변환하기/convert-to-decimal.kt
@@ -1,14 +1,6 @@
 val binarySeq = readln().map {it.toInt() - 48}
 
-var ans = 0
-
 fun main() {
-    if(binarySeq.size == 1) {
-        println(binarySeq[0])
-        return
-    }
-    binarySeq.forEach { 
-        ans = 2 * ans + it
-    }
+    val ans = binarySeq.fold(0) { acc, bit -> acc * 2 + bit }
     println(ans)
 }
