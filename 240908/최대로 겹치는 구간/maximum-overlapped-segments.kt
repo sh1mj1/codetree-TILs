afd51a13e6fc5d242034a288160f75a9beb99eaa
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-09-08 23:49:14 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240908/최대로 겹치는 구간/maximum-overlapped-segments.kt
@@ -0,0 +1,22 @@
+// 점은 -100 ~ 100 까지 찍힐 수 있ㅇㅁ.
+// -> 점 찍힐 때 100 씩 더하기.
+
+val n = readln().toInt()
+
+val interval = IntArray(202) { 0 }
+var ans = 0
+
+fun main() {
+    repeat(3) {
+        val line = readln().split(" ").map { it.toInt() }
+        val x1 = line[0] + 100
+        val x2 = line[1] + 100
+        for(i in x1+1 .. x2) {
+            arr[i]++
+            if(ans < arr[i]) {
+                ans++
+            }
+        }
+    }
+    println(ans)
+}
