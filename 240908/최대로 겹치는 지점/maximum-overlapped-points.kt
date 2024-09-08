3032a0f89ae39cd41f0a269836266fc6c67e753f
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-09-08 23:55:51 +0900

    Title: 최대로 겹치는 지점, Time: 144ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240908/최대로 겹치는 지점/maximum-overlapped-points.kt
@@ -0,0 +1,20 @@
+val n = readln().toInt()
+
+val interval = IntArray(101) { 0 }
+var ans = 0
+
+fun main() {
+    repeat(n) {
+        val line = readln().split(" ").map { it.toInt() }
+        println(line.toString())
+        val x1 = line[0]
+        val x2 = line[1]
+        for(i in x1 .. x2) {
+            interval[i]++
+            if(ans < interval[i]) {
+                ans++
+            }
+        }
+    }
+    println(ans)
+}
