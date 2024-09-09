4529621c519ef5af1a44e40f9ec95e12cf4d142f
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-09-10 08:38:53 +0900

    Title: 흰검 칠하기, Time: 202ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240910/흰검 칠하기/painting-white-black.kt
@@ -0,0 +1,81 @@
+var ans = 0
+val n = readln().toInt()
+
+val sections = List(200_002) { Section(0, 0, 0) }
+var nowPoint = 100_000
+
+var mostRightPoint = 100_000
+var mostLeftPoint = 100_000
+
+fun main() {
+
+    repeat(n) {
+        val cmd = readln().split(" ")
+        val count = cmd[0].toInt()
+        val direction = cmd[1]
+
+        when(direction) {
+            "L" -> {
+                for(i in 0 until count) {
+                    val nowSection = sections[nowPoint]
+                    if(nowSection.recent == 3) {
+                        nowPoint--
+                        continue
+                    }
+                    nowSection.white++
+
+                    if(nowSection.black >=2 && nowSection.white >= 2) {
+                        nowSection.recent = 3
+                    }else {
+                        nowSection.recent = 2
+                    }
+                    if(i == count-1) {
+                        break
+                    }
+                    nowPoint--
+                }
+            }
+            "R" -> {
+                for(i in 0 until count) {
+                    val nowSection = sections[nowPoint]
+                    if(nowSection.recent == 3 ) {
+                        nowPoint++
+                        continue
+                    }
+                    
+                    nowSection.black++
+                    if(nowSection.black >=2 && nowSection.white >= 2) {
+                        nowSection.recent = 3
+                    }else {
+                        nowSection.recent = 1
+                    }
+                    if(i == count-1) {
+                        break
+                    }
+                    nowPoint++
+                }
+            }
+            else -> {}
+        }
+    }
+
+    var white = 0
+    var black = 0
+    var gray = 0
+    for(section in sections) {
+        when(section.recent) {
+            1 -> {black++}
+            2 -> {white++}
+            3 -> {gray++}
+            else -> {}
+        }
+    }
+    println("$white $black $gray")
+}
+
+data class Section(
+    var black: Int,
+    var white: Int,
+    // 0: none, 1: black, 2: white, 3: gray
+    var recent: Int
+)
