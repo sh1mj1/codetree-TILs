85d2f25f96be6ae000cc59d77566e4c00a72b5d8
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-10-13 23:32:28 +0900

    Title: 중앙값 계산 2, Time: 166ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241013/중앙값 계산 2/get-median-2.kt
+++ 241013/중앙값 계산 2/get-median-2.kt
@@ -2,7 +2,7 @@ val n = readln().toInt()
 val numbers = readln().split(" ").map { it.toInt() }
 
 fun main() {
-    numbers.forEachIndexed{ idx, number ->
+    numbers.sorted().forEachIndexed{ idx, number ->
         if(idx % 2 == 0) {
             val midIdx = (idx+1) / 2
             print("${numbers[midIdx]} ")
