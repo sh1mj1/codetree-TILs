0ee887735ac97e383ec225eb88492d8197857ebc
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2024-12-20 09:39:58 +0900

    Title: 정수 명령 처리 5, Time: 279ms, Memory: 15MB, Status: Passed - CodeTree

--- 241220/정수 명령 처리 5/process-numeric-commands-5.kt
+++ 241220/정수 명령 처리 5/process-numeric-commands-5.kt
@@ -6,7 +6,7 @@ fun main() {
         val command = readln().split(" ")
         when(command[0]) {
             "push_back" -> arr.add(command[1].toInt())
-            "pop_back" -> arr.remove(arr.size-1)
+            "pop_back" -> arr.removeAt(arr.size -1)
             "size" -> println(arr.size)
             "get" -> println(arr[command[1].toInt() - 1])
             else -> error("Illegal input")
