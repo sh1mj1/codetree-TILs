a2d5564886a07339fa57a09e0f76b65fb57f69da
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-12 00:15:52 +0900

    Title: 1이 3개 이상 있는 위치, Time: 131ms, Memory: 12MB, Status: Runtime Error - CodeTree

---
+++ 250212/1이 3개 이상 있는 위치/place-more-than-3-ones.kt
@@ -0,0 +1,37 @@
+
+
+fun main() {
+    val n = readln().toInt()
+    val grid = List(n) {
+        readln().split(" ").map { it.toInt() }
+    }
+    val range = 0 until n
+    val directions = listOf(
+        1 to 0, // DOWN
+        0 to -1, // LEFT
+        -1 to 0, // UP
+        0 to 1, // RIGHT
+    )
+
+    var count = 0
+
+    for (r in range) {
+        for (c in range) {
+            var currentCount = 0
+            for (dir in directions) {
+                val currentR = r + dir.first
+                val currentC = c + dir.second
+
+                if (currentR !in range) continue
+                if (currentC !in range) continue
+                
+                if (grid[currentR][currentC] == 1) {
+                    currentCount++
+                }
+            }
+            if (currentCount >= 3) count++
+        }
+    }
+    println(count)
+}
+
