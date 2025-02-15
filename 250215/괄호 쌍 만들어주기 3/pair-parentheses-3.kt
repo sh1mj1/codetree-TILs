155c4aea8ac42b342daf8a516330042871e21bc7
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-15 13:42:27 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 126ms, Memory: 12MB, Status: Passed - CodeTree

--- 250215/괄호 쌍 만들어주기 3/pair-parentheses-3.kt
+++ 250215/괄호 쌍 만들어주기 3/pair-parentheses-3.kt
@@ -4,15 +4,12 @@ fun main() {
     val input = readln().trim()
     val lastIdx = input.length - 1
 
-    var count = 0
-    (0 .. lastIdx).forEach { i ->
-        if (input[i] == '(') {
-            (i + 1 .. lastIdx).forEach { j ->
-                if (input[j] == ')') {
-                    count++
-                }
+    val count = input.withIndex().sumOf { (i, b1) -> 
+        if ( b1 == '(') {
+            (i + 1 .. lastIdx).count { j ->
+                input[j] == ')'
             }
-        }
+        } else 0
     }
 
     println(count)
