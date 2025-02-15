88cd89ff6fbed0d1cba57bd3dbfba6db9c25ae14
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-15 14:53:03 +0900

    Title: 체크판위에서 2, Time: 163ms, Memory: 15MB, Status: Passed - CodeTree

--- 250215/체크판위에서 2/on-the-checkboard-2.kt
+++ 250215/체크판위에서 2/on-the-checkboard-2.kt
@@ -7,6 +7,7 @@ fun main() {
         readln().trim().split(" ")
     }
     val firstColor = grid[0][0]
+    val lastColor = grid[rowCount - 1][colCount - 1]
 
     val count = 
     (1 until rowCount - 2).sumOf { r1 ->
@@ -17,7 +18,7 @@ fun main() {
             (r1 + 1 until rowCount - 1).sumOf { r2 ->
                 (c1 + 1 until colCount - 1).count { c2 ->
                     val color2 = grid[r2][c2]
-                    color2.isDifferentWith(color1)
+                    color2.isDifferentWith(color1) && color2.isDifferentWith(lastColor)
                 }
             }
         }
