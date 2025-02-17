bd9e184b5eb33ce1efe383b21c0e1e80c87ba38f
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-18 01:03:35 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 197ms, Memory: 18MB, Status: Passed - CodeTree

--- 250218/두 가지로 열리는 자물쇠/a-two-way-lock.kt
+++ 250218/두 가지로 열리는 자물쇠/a-two-way-lock.kt
@@ -12,14 +12,6 @@ val adjacents = List(n + 1) { i ->
     )
 }
 
-val range = 1..n
-val numbersSet = (1..n).map{ it }.toSet()
-
-fun main() {
-    val validPasswords = generateValidPasswords(password1) union generateValidPasswords(password2)
-    println(validPasswords.size)
-}
-
 private fun adjacentNumber(number: Int, distance: Int): Int {
     return ((number + distance + n) % n).let {
         if (it == 0) n
@@ -27,14 +19,34 @@ private fun adjacentNumber(number: Int, distance: Int): Int {
     }
 }
 
-fun generateValidPasswords(password: List<Int>): Set<Triple<Int, Int, Int>> {
-    val validPasswords = mutableSetOf<Triple<Int, Int, Int>>()
-    for (i in adjacents[password[0]]) {
-        for (j in adjacents[password[1]]) {
-            for (k in adjacents[password[2]]) {
-                validPasswords.add(Triple(i, j, k))
+fun main() {
+    val validPasswords = validPasswords(password1) union validPasswords(password2)
+    println(validPasswords.size)
+}
+
+
+
+fun validPasswords(password: List<Int>): List<Triple<Int, Int, Int>> {
+    return adjacents[password[0]].flatMap { i ->
+        adjacents[password[1]].flatMap { j ->
+            adjacents[password[2]].map { k ->
+                Triple(i, j, k)
             }
         }
     }
-    return validPasswords
 }
+
+
+/*
+
+// 2️⃣ 가능한 조합을 함수형 API로 생성 (`O(1)`)
+fun generateValidPasswords(password: List<Int>) =
+    adjacents[password[0]].flatMap { i ->
+        adjacents[password[1]].flatMap { j ->
+            adjacents[password[2]].map { k ->
+                Triple(i, j, k)
+            }
+        }
+    }.toSet() // `Set`으로 변환하여 중복 제거
+
+*/
