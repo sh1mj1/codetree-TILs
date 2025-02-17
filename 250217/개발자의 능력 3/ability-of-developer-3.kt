ad8fcd61529c62fd4f696a01662c87a57d238ed4
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-17 19:49:38 +0900

    Title: 개발자의 능력 3, Time: 153ms, Memory: 15MB, Status: Passed - CodeTree

--- 250217/개발자의 능력 3/ability-of-developer-3.kt
+++ 250217/개발자의 능력 3/ability-of-developer-3.kt
@@ -1,12 +1,11 @@
 import kotlin.math.abs
 
-
 fun main() {
     val abilities = readln().trim().split(" ").map(String::toInt)
     val totalSum = abilities.sum()
     val size = abilities.size
 
-    val asnwer = (0 until size - 2).minOf { i ->
+    val answer = (0 until size - 2).minOf { i ->
         (i + 1 until size - 1).minOf { j ->
             (j + 1 until size).minOf { k ->
                 val teamSum = abilities[i] + abilities[j] + abilities[k]
