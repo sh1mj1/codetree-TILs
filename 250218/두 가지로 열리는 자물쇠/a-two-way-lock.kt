322369feb2b1efc99a120eb1a5a26bcc48970e32
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-18 01:11:10 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 218ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 250218/두 가지로 열리는 자물쇠/a-two-way-lock.kt
+++ 250218/두 가지로 열리는 자물쇠/a-two-way-lock.kt
@@ -2,34 +2,32 @@ val n = readln().toInt()
 val password1 = readln().trim().split(" ").map(String::toInt)
 val password2 = readln().trim().split(" ").map(String::toInt)
 
-val adjacents = List(n + 1) { i ->
-    setOf(
-        adjacentNumber(i, -2),
-        adjacentNumber(i, -1),
-        i,
-        adjacentNumber(i, 1),
-        adjacentNumber(i, 2),
-    )
-}
-
-private fun adjacentNumber(number: Int, distance: Int): Int {
-    return ((number + distance + n) % n).let {
-        if (it == 0) n
-        else it
-    }
-}
+fun adjacentNumbers(x: Int) = setOf(
+    (x - 2 + n) % n.let { if (it == 0) n else it },
+    (x - 1 + n) % n.let { if (it == 0) n else it },
+    x,
+    (x + 1) % n.let { if (it == 0) n else it },
+    (x + 2) % n.let { if (it == 0) n else it }
+)
 
 fun main() {
-    val validPasswords = validPasswords(password1) union validPasswords(password2)
-    println(validPasswords.size)
-}
+    val validPasswords = mutableSetOf<Triple<Int, Int, Int>>()
 
-fun validPasswords(password: List<Int>): Set<Triple<Int, Int, Int>> {
-    return adjacents[password[0]].flatMap { i ->
-        adjacents[password[1]].flatMap { j ->
-            adjacents[password[2]].map { k ->
-                Triple(i, j, k)
+    for (i in adjacentNumbers(password1[0])) {
+        for (j in adjacentNumbers(password1[1])) {
+            for (k in adjacentNumbers(password1[2])) {
+                validPasswords.add(Triple(i, j, k))
             }
         }
-    }.toSet()
+    }
+
+    for (i in adjacentNumbers(password2[0])) {
+        for (j in adjacentNumbers(password2[1])) {
+            for (k in adjacentNumbers(password2[2])) {
+                validPasswords.add(Triple(i, j, k))
+            }
+        }
+    }
+
+    println(validPasswords.size)
 }
