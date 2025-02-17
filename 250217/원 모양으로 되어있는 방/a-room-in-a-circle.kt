e65f1ab707dc34193723f878e4e16fa3f8b2cb76
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-17 18:23:53 +0900

    Title: 원 모양으로 되어있는 방, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250217/원 모양으로 되어있는 방/a-room-in-a-circle.kt
+++ 250217/원 모양으로 되어있는 방/a-room-in-a-circle.kt
@@ -2,30 +2,15 @@ import kotlin.math.min
 
 fun main() {
     val n = readln().toInt()
-    val maxIdx = n - 1
+    val people = List(n) { readln().toInt() }
 
-    val people = List(n) {
-        readln().toInt()
-    }
-    val peopleSum = people.sumOf { it }
 
-    var startIdx = 0
-    val peopleCombinations: List<List<Int>> = List(n) {
-        List(n) {
-            people[startIdx].also { startIdx = increasedIdx(startIdx, maxIdx) }
-        }.also { startIdx++ }
-    }
-
-    val answer = peopleCombinations.minOf { p ->
-        p.withIndex().sumOf { (idx, value) -> idx * value }
+    val answer = (0 until n).minOf { startIdx ->
+        people.withIndex().sumOf { (idx, peopleCount) -> 
+            val targetIdx = (idx + startIdx) % n
+            targetIdx * value
+        }
     }
 
     println(answer)
-}
-
-fun increasedIdx(index: Int, maxIdx: Int): Int {
-    if (index >= maxIdx) {
-        return index - maxIdx
-    } 
-    return index + 1
 }
