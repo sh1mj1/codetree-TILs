2e354dd3ccd4b6cfd155c30428c226837892adef
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-18 08:56:56 +0900

    Title: 숫자 카운트, Time: 192ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 250218/숫자 카운트/numeric-count.kt
@@ -0,0 +1,60 @@
+
+
+fun main() {
+    val n = readln().toInt()
+
+    val queries = List(n) {
+        val (number, strike, ball) = readln().trim().split(" ").map { it }
+        Query(
+            number,
+            strike.toInt(),
+            ball.toInt(),
+        )
+    }
+
+    val allNumbers = mutableListOf<List<Int>>()
+
+    for (i in 1 .. 9) {
+        for (j in 1.. 9) {
+            if (j != i) {
+                for (k in 1 .. 9) {
+                    if (k != j && k != i) {
+                        allNumbers.add(listOf(i, j, k))
+                    }
+                }
+            }
+        }
+    }
+
+    allNumbers.count { numbers ->
+        queries.all { query -> 
+            query.possible(numbers)
+        }
+    }.let(::println)
+}
+
+
+
+data class Query(
+    val number: String,
+    val strike: Int,
+    val ball: Int,
+) {
+    val digits = number.map {
+        it.digitToInt()
+    }
+
+    fun possible(target: List<Int>): Boolean {
+        var strikeCount = 0
+        var ballCount = 0
+
+        for (i in 0 .. 2) {
+            if (digits[i] == target[i]) strikeCount++
+        }
+        ballCount = digits.intersect(target).count() - strikeCount
+    
+        if (strikeCount == strike && ballCount == ball) return true
+
+        return false
+    }
+}
