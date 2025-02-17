3c1f431a6b828f4ad618c427b4f9a9b96a87dc74
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-17 19:23:44 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 159ms, Memory: 15MB, Status: Passed - CodeTree

--- 250217/한 가지로 열리는 자물쇠/one-way-lock.kt
+++ 250217/한 가지로 열리는 자물쇠/one-way-lock.kt
@@ -4,23 +4,14 @@ fun main() {
     val n = readln().toInt()
     val (x, y, z) = readln().trim().split(" ").map(String::toInt)
 
-    val a = (1 until n).count {
-        abs(it - x) <= 2
-    }
+    val a = (1 .. n).count { abs(it - x) <= 2 }
+    val b = (1 .. n).count { abs(it - y) <= 2 }
+    val c = (1 .. n).count { abs(it - z) <= 2 }
 
-    val b = (1 until n).count {
-        abs(it - y) <= 2
-    }
+    val allCombinationCount = n * n * n
+    val wrongCombinationCount = (n - a) * (n - b) * (n - c)
 
-    val c = (1 until n).count {
-        abs(it - z) <= 2
-    }
-
-    val allValidCount = n * n * (a + b + c)
-    val onlyTwoValidCount = n * (a * b + a * c + b * c)
-    val onlyOneValidCount = a * b * c
-
-    (allValidCount - onlyTwoValidCount + onlyOneValidCount).let {
+    (allCombinationCount - wrongCombinationCount).let {
         println(it)
     }
 
