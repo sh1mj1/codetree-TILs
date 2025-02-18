dd55e3b39434b63051980952ada9a0795e7956a8
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-18 09:40:56 +0900

    Title: 개발자의 능력 2, Time: 178ms, Memory: 17MB, Status: Passed - CodeTree

--- 250218/개발자의 능력 2/ability-of-developer-2.kt
+++ 250218/개발자의 능력 2/ability-of-developer-2.kt
@@ -1,4 +1,3 @@
-import kotlin.math.min
 
 fun main() {
     val devs = readln().trim().split(" ").map(String::toInt)
@@ -6,22 +5,19 @@ fun main() {
 
     val range = 0 .. 5
 
-    var minDiff = Int.MAX_VALUE
-
-    range.forEach { i ->
-        range.filter { it != i}.forEach { j ->
+    range.flatMap { i ->
+        range.filter { it != i}.flatMap { j ->
             val sum1 = devs[i] + devs[j]
-
-            range.filter { it != i && it != j }.forEach { k ->
-                range.filter { it != i && it != j && it != k }.forEach { l ->
+            
+            range.filter { it != i && it != j }.flatMap { k ->
+                range.filter { it != i && it != j && it != k }.map { l ->
                     val sum2 = devs[k] + devs[l]
                     val sum3 = totalSum - sum1 - sum2
-
                     val sums = listOf(sum1, sum2, sum3)
-                    minDiff = min(minDiff, sums.maxOf{it} - sums.minOf{it})
+                    
+                    sums.maxOf { it } - sums.minOf { it }
                 }
             } 
         } 
-    }
-    println(minDiff)
+    }.minOf{it}.let(::println)
 }
