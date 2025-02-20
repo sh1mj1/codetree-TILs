cac93cda60b754c2cb2b265c6335dbd207f7379a
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-20 19:03:05 +0900

    Title: 운행 되고 있는 시간, Time: 219ms, Memory: 23MB, Status: Passed - CodeTree

--- 250220/운행 되고 있는 시간/hours-in-service.kt
+++ 250220/운행 되고 있는 시간/hours-in-service.kt
@@ -1,36 +1,19 @@
-import kotlin.math.min
 import kotlin.math.max
 
 fun main() {
     val n = readln().toInt()
 
-    var startOperatingTime = 0
-    var endOperatingTime = 1_000
-
     val workers = List(n) {
-        val (x, y) = readln().trim().split(" ").map(String::toInt)
-        startOperatingTime = min(x, startOperatingTime)
-        endOperatingTime = max(y, startOperatingTime)
-        Worker(x, y)
+        val (a, b) = readln().trim().split(" ").map(String::toInt)
+        Worker(a, b)
     }
 
-    var maxOperatingTime = 0
-    var workTimes = MutableList(1_001) { 0 }
-
-    for (i in 0 until n) {
-        var currentOperatingTime = 0
-        for (j in 0 until n) {
-            if (i != j) {
-                with(workers[j]) {
-                    workTimeRange.forEach {
-                        if (workTimes[it] == 0) currentOperatingTime++
-                        workTimes[it]++
-                    }
-                }
-            }
-        }
-        workTimes = MutableList(1_001) { 0 }
-        maxOperatingTime = max(maxOperatingTime, currentOperatingTime)        
+    val maxOperatingTime = workers.indices.maxOf { i ->
+        workers
+            .filterIndexed { index, _ -> index != i }
+            .flatMap { it.workTimeRange }
+            .toSet()
+            .size
     }
 
     println(maxOperatingTime)
@@ -40,5 +23,5 @@ data class Worker(
     val workStartTime: Int,
     val workEndTime: Int,
 ) {
-    val workTimeRange = (workStartTime until workEndTime)
+    val workTimeRange: Set<Int> = (workStartTime until workEndTime).toSet()
 }
