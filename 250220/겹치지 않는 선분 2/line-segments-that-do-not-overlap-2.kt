cc082a7fd8fa843e30855ab68cb4fe0ae823838a
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-20 20:35:25 +0900

    Title: 겹치지 않는 선분 2, Time: 138ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
+++ 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
@@ -1,16 +1,17 @@
-
+import kotlin.math.abs
 
 fun main() {
     val n = readln().toInt()
 
     val lines = List(n) {
         val (x1, x2) = readln().trim().split(" ").map(String::toInt)
-        Line(Point(x1, 0),Point(x2, 1))
+        val (start, end) = if (x1 <= x2) x1 to x2 else x2 to x1
+        Line(Point(start, 0), Point(end, 1))
     }
 
     lines.count { stand ->
         lines
-            .filter { it != stand}
+            .filter { it != stand }
             .any { it.isOverlapped(stand) }
     }.let(::println)
 }
@@ -19,24 +20,15 @@ data class Line(
     val p1: Point,
     val p2: Point
 ) {
-    val slope: Float = p2.x.toFloat() - p1.x.toFloat()
+    val slope: Float = (p2.x - p1.x).toFloat()
     val xIntercept: Int = p1.x
 
     fun isOverlapped(other: Line): Boolean {
-        if (slope == other.slope) return false // 평행한 경우
+        if (slope == other.slope) return false
 
-        val intersectY = (other.xIntercept.toFloat() - xIntercept.toFloat()) / (slope - other.slope)
+        val overlappedPointY = (other.xIntercept.toFloat() - xIntercept.toFloat()) / (slope - other.slope)
 
-        if (intersectY <= 0 || intersectY >= 1) return false // y 값이 범위 내에 있어야 함
-
-        val intersectX = xIntercept + slope * intersectY
-
-        val minX1 = minOf(p1.x, p2.x)
-        val maxX1 = maxOf(p1.x, p2.x)
-        val minX2 = minOf(other.p1.x, other.p2.x)
-        val maxX2 = maxOf(other.p1.x, other.p2.x)
-
-        return intersectX in minX1..maxX1 && intersectX in minX2..maxX2
+        return overlappedPointY > 0.0 && overlappedPointY < 1
     }
 }
 
