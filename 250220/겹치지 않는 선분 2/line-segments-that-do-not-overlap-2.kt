6944e505713b3bda31f2e4f59e14164e258a675c
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-20 20:59:10 +0900

    Title: 겹치지 않는 선분 2, Time: 154ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
+++ 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
@@ -18,6 +18,11 @@ data class Line(
     val x2: Int,
 ) {
     fun isOverlapped(other: Line): Boolean {
-        return (x1 <= other.x1 && x2 >= other.x2 ) || (x1 >= other.x1 && x2 <= other.x2)
+        val minX1 = minOf(x1, x2)
+        val maxX1 = maxOf(x1, x2)
+        val minX2 = minOf(other.x1, other.x2)
+        val maxX2 = maxOf(other.x1, other.x2)
+
+    return (minX1 <= minX2 && maxX1 >= maxX2) || (minX1 >= minX2 && maxX1 <= maxX2)
     }
 }
