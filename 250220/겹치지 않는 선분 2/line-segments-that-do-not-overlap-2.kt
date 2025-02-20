36c39a4b9a0f3775abcbdfa22c4d5d8954db0a45
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-20 21:02:48 +0900

    Title: 겹치지 않는 선분 2, Time: 147ms, Memory: 14MB, Status: Passed - CodeTree

--- 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
+++ 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
@@ -1,38 +1,36 @@
-import java.util.Scanner
-
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val n = scanner.nextInt()
+    val n = readln().toInt()
 
-    val x1 = IntArray(n)
-    val x2 = IntArray(n)
-
-    for (i in 0 until n) {
-        x1[i] = scanner.nextInt()
-        x2[i] = scanner.nextInt()
+    val lines = List(n) {
+        val (x1, x2) = readln().trim().split(" ").map(String::toInt)
+        Line(Point(x1, 0),Point(x2, 0))
     }
 
-    var ans = 0
-
-    // 다른 선분과 겹치지 않는 선분의 수를 구합니다.
-    for (i in 0 until n) {
-        // i번째 선분이 다른 선분과 겹치지 않는지 확인합니다.
-        var overlap = false
-
-        for (j in 0 until n) {
-            // 자기 자신은 제외합니다.
-            if (j == i) continue
-
-            // x1이 큰 쪽 선분이 x2가 더 작다면 겹치게 됩니다.
-            if ((x1[i] <= x1[j] && x2[i] >= x2[j]) || (x1[i] >= x1[j] && x2[i] <= x2[j])) {
-                overlap = true
-                break
-            }
-        }
-
-        // 겹치지 않았다면 정답의 개수에 하나를 추가합니다.
-        if (!overlap) ans++
-    }
-
-    println(ans)
+    lines.count { stand ->
+        lines
+            .filter { it != stand}
+            .all { !it.isOverlapped(stand) }
+    }.let(::println)
 }
+
+data class Line(
+    val p1: Point,
+    val p2: Point
+) {
+    val slope: Float = p2.x.toFloat() - p1.x.toFloat()
+    val yIntercept: Int = p1.x
+
+    fun isOverlapped(other: Line): Boolean {
+        if (slope == other.slope) return false
+
+        val overlappedPointY = (other.yIntercept.toFloat() - yIntercept.toFloat()) / (slope - other.slope)
+
+        return overlappedPointY > 0.0 && overlappedPointY < 1
+    }
+}
+
+data class Point(
+    val x: Int, 
+    val y: Int,
+)
+
