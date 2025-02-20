2535dc1f5f73bf085155379947ab854f0cc7ea31
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-20 20:20:24 +0900

    Title: 겹치지 않는 선분 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
+++ 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
@@ -22,21 +22,22 @@ data class Line(
     val slope: Float = p2.x.toFloat() - p1.x.toFloat()
     val xIntercept: Int = p1.x
 
- fun isOverlapped(other: Line): Boolean {
-    if (slope == other.slope) return false // 평행한 경우
+    fun isOverlapped(other: Line): Boolean {
+        if (slope == other.slope) return false // 평행한 경우
 
-    val intersectY = (other.xIntercept.toFloat() - xIntercept.toFloat()) / (slope - other.slope)
+        val intersectY = (other.xIntercept.toFloat() - xIntercept.toFloat()) / (slope - other.slope)
 
-    if (intersectY <= 0 || intersectY >= 1) return false // y 값이 범위 내에 있어야 함
+        if (intersectY <= 0 || intersectY >= 1) return false // y 값이 범위 내에 있어야 함
 
-    val intersectX = xIntercept + slope * intersectY
+        val intersectX = xIntercept + slope * intersectY
 
-    val minX1 = minOf(p1.x, p2.x)
-    val maxX1 = maxOf(p1.x, p2.x)
-    val minX2 = minOf(other.p1.x, other.p2.x)
-    val maxX2 = maxOf(other.p1.x, other.p2.x)
+        val minX1 = minOf(p1.x, p2.x)
+        val maxX1 = maxOf(p1.x, p2.x)
+        val minX2 = minOf(other.p1.x, other.p2.x)
+        val maxX2 = maxOf(other.p1.x, other.p2.x)
 
-    return intersectX in minX1..maxX1 && intersectX in minX2..maxX2
+        return intersectX in minX1..maxX1 && intersectX in minX2..maxX2
+    }
 }
 
 data class Point(
