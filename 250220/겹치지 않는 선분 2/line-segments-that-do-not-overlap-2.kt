53781b0fd27c0573f598d85d09c17c6fa24956fe
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-20 20:57:12 +0900

    Title: 겹치지 않는 선분 2, Time: 151ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
+++ 250220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.kt
@@ -1,38 +1,23 @@
-import kotlin.math.abs
-
 fun main() {
     val n = readln().toInt()
 
     val lines = List(n) {
         val (x1, x2) = readln().trim().split(" ").map(String::toInt)
-        val (start, end) = if (x1 <= x2) x1 to x2 else x2 to x1
-        Line(Point(start, 0), Point(end, 1))
+        Line(x1, x2)
     }
 
-    lines.count { stand ->
-        lines
-            .filter { it != stand }
-            .any { it.isOverlapped(stand) }
+    lines.count { standard ->
+        lines.any {
+            it != standard && standard.isOverlapped(it)
+        }
     }.let(::println)
 }
 
 data class Line(
-    val p1: Point,
-    val p2: Point
+    val x1: Int,
+    val x2: Int,
 ) {
-    val slope: Float = (p2.x - p1.x).toFloat()
-    val xIntercept: Int = p1.x
-
     fun isOverlapped(other: Line): Boolean {
-        if (slope == other.slope) return false
-
-        val overlappedPointY = (other.xIntercept.toFloat() - xIntercept.toFloat()) / (slope - other.slope)
-
-        return overlappedPointY > 0.0 && overlappedPointY < 1
+        return (x1 <= other.x1 && x2 >= other.x2 ) || (x1 >= other.x1 && x2 <= other.x2)
     }
 }
-
-data class Point(
-    val x: Int, 
-    val y: Int,
-)
