2a0bec0a6d63ba9c15e6642de769c226b762a427
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-23 22:31:51 +0900

    Title: 상해버린 치즈, Time: 195ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250223/상해버린 치즈/rotten-cheese.kt
@@ -0,0 +1,38 @@
+
+
+fun main() {
+    val (peopleCount, cheeseCount, eatCount, hurtCount) = readln().trim().split(" ").map(String::toInt)
+    val eatHistories = List(eatCount) {
+        val (personIdx, cheeseIdx, eatTime) = readln().trim().split(" ").map(String::toInt)
+        EatHistory(personIdx, cheeseIdx, eatTime)
+    }
+
+    val hurtHistories = List(hurtCount) {
+        val (personIdx, hurtTime) = readln().trim().split(" ").map(String::toInt)
+        HurtHistory(personIdx, hurtTime)
+    }
+
+    val hurtPeople = hurtHistories.map { it.personIdx }.toMutableList()
+
+    eatHistories.forEach { eatHistory ->
+        if (!hurtPeople.contains(eatHistory.personIdx)) {
+            hurtPeople.add(eatHistory.personIdx)
+        }
+        if (hurtPeople.count() == peopleCount) {
+            return@forEach
+        }
+    }
+    println(hurtPeople.count())
+
+}
+
+data class EatHistory(
+    val personIdx: Int,
+    val cheeseIdx: Int,
+    val eatTime: Int,
+)
+
+data class HurtHistory(
+    val personIdx: Int,
+    val hurtTime: Int,
+)
