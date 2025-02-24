d35c626cf19e8f884e6248256e8b3404718b2ce7
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-24 17:04:38 +0900

    Title: 선분 3개 지우기, Time: 146ms, Memory: 14MB, Status: Passed - CodeTree

--- 250224/선분 3개 지우기/remove-three-segments.kt
+++ 250224/선분 3개 지우기/remove-three-segments.kt
@@ -1,43 +1,35 @@
-
-
 fun main() {
     val lineCount = readln().toInt()
-    val baseLine = List(101) { mutableListOf<Line>() }
-
     val lines = List(lineCount) {
         readln().trim().split(" ").map(String::toInt).let { Line(it[0], it[1]) }
     }
-    var combinationCount = 0
 
-    lines.forEach { line ->
-        (line.start .. line.end).forEach {
-            baseLine[it].add(line)
-        }
-    }
+    var combinationsCount = 0
 
     for (i1 in 0 until lineCount - 2) {
         for (i2 in i1 + 1 until lineCount - 1) {
             for (i3 in i2 + 1 until lineCount) {
-                val line1 = lines[i1]
-                val line2 = lines[i2]
-                val line3 = lines[i3]
+                val remainedLines = lines.filterIndexed { index, _ -> index != i1 && index != i2 && index != i3 }
 
-            val copiedBaseLine = baseLine.map { it.toMutableList() }
-
-                (line1.start..line1.end).forEach { copiedBaseLine[it].remove(line1) }
-                (line2.start..line2.end).forEach { copiedBaseLine[it].remove(line2) }
-                (line3.start..line3.end).forEach { copiedBaseLine[it].remove(line3) }
-
-                
-                if (copiedBaseLine.all { it.size == 1 || it.isEmpty() }) combinationCount++
+                if (remainedLines.all { line1 ->
+                        remainedLines.all { line2 ->
+                            line1 == line2 || !line1.isOverlapped(line2)
+                        }
+                    }) {
+                    combinationsCount++
+                }
             }
         }
     }
 
-    println(combinationCount)
+    println(combinationsCount)
 }
 
 data class Line(
     val start: Int,
     val end: Int,
-)
+) {
+    fun isOverlapped(other: Line): Boolean {
+        return end >= other.start && start <= other.start
+    }
+}
