6bad7c59b374b9509c190b1d3f538afed3da5dcb
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-24 14:43:54 +0900

    Title: 개발자의 순위, Time: 165ms, Memory: 15MB, Status: Passed - CodeTree

--- 250224/개발자의 순위/developers-rank.kt
+++ 250224/개발자의 순위/developers-rank.kt
@@ -1,20 +1,53 @@
-
-
 fun main() {
     val (matchCount, devCount) = readln().trim().split(" ").map(String::toInt)
     val matchResults = List(matchCount) { readln().trim().split(" ").map(String::toInt) }
 
+    val firstMatchOrder = matchResults.first()
 
-    val allDevPairCount = (1 .. devCount).sumOf { standardDev ->
-        (standardDev + 1 .. devCount).count { targetDev -> 
-            val (highPriority, lowPriority) = 
-                if (matchResults.first().let { result -> result.indexOf(standardDev) > result.indexOf(targetDev) } ) 
-                    targetDev to standardDev
-                else
-                    standardDev to targetDev
-            
-            matchResults.all { result -> result.indexOf(highPriority) < result.indexOf(lowPriority) }
+    var totalImmutablePairs = 0
+
+    for (standardDev in 1 until devCount) {
+        for (targetDev in standardDev + 1 .. devCount) {
+            val highPriority: Int
+            val lowPriority: Int
+
+            if (isHighPriority(standardDev, targetDev, firstMatchOrder)) {
+                highPriority = standardDev
+                lowPriority = targetDev
+            } else {
+                highPriority = targetDev
+                lowPriority = standardDev
+            }
+
+            var isImmutable = true
+            for (match in matchResults) {
+                if (!isHighPriority(highPriority, lowPriority, match)) {
+                    isImmutable = false
+                    break
+                }
+            }
+
+            if (isImmutable) totalImmutablePairs++
         }
     }
-    println(allDevPairCount)
+
+    println(totalImmutablePairs)
 }
+
+fun isHighPriority(high: Int, low: Int, source: List<Int>): Boolean {
+    var highAppear = false
+    var lowAppear = false
+
+    source.forEach {
+        if (it == high) highAppear = true
+        if (it == low) lowAppear = true
+
+        if (lowAppear && !highAppear) return false
+        if (highAppear && !lowAppear) return true
+    }
+
+    return false
+}
+
+
+
