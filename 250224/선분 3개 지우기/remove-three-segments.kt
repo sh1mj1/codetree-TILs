f4ee239842fc1ae8acb09beb3353e8992e1fcb8b
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-24 17:08:36 +0900

    Title: 선분 3개 지우기, Time: 152ms, Memory: 15MB, Status: Passed - CodeTree

--- 250224/선분 3개 지우기/remove-three-segments.kt
+++ 250224/선분 3개 지우기/remove-three-segments.kt
@@ -4,20 +4,14 @@ fun main() {
         readln().trim().split(" ").map(String::toInt).let { Line(it[0], it[1]) }
     }
 
-    var combinationsCount = 0
-
-    for (i1 in 0 until lineCount - 2) {
-        for (i2 in i1 + 1 until lineCount - 1) {
-            for (i3 in i2 + 1 until lineCount) {
+    val combinationsCount = (0 until lineCount - 2).sumOf { i1 ->
+        (i1 + 1 until lineCount - 1).sumOf { i2 ->
+            (i2 + 1 until lineCount).count { i3 ->
                 val remainedLines = lines.filterIndexed { index, _ -> index != i1 && index != i2 && index != i3 }
 
-                if (remainedLines.all { line1 ->
-                        remainedLines.all { line2 ->
-                            line1 == line2 || !line1.isOverlapped(line2)
-                        }
-                    }) {
-                    combinationsCount++
-                }
+                remainedLines.all { line1 ->
+                    remainedLines.all { line2 -> line1 == line2 || !line1.isOverlapped(line2) }
+                } 
             }
         }
     }
