1cf80c08be1614e52b107b06e4928438dc8902ed
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-24 17:47:48 +0900

    Title: 흥미로운 숫자 2, Time: 619ms, Memory: 52MB, Status: Passed - CodeTree

--- 250224/흥미로운 숫자 2/interesting-numbers-2.kt
+++ 250224/흥미로운 숫자 2/interesting-numbers-2.kt
@@ -4,22 +4,14 @@ fun main() {
     val (startNumber, endNumber) = readln().trim().split(" ").map(String::toInt)
 
     val interestingNumberCount = (startNumber .. endNumber).count(Int::isInteresting)
-    
+
     println(interestingNumberCount)
 }
 
-private fun Int.isInteresting(): Boolean {
-    val digits = this.toString().map { it.digitToInt() }
+fun Int.isInteresting(): Boolean {
+    val digits = this.toString().map { it }
 
-    val iteratedDigit: Int
-
-    if (digits[0] == digits[1]) {
-        iteratedDigit = digits[0]
-    } else if (digits[1] == digits[2]) {
-        iteratedDigit = digits[1]
-    } else {
-        iteratedDigit = digits[0]
+    return digits.toSet().size == 2 && 
+        digits.any { digit -> digits.count { it == digit } == digits.size - 1 
     }
-
-    return digits.count { it != iteratedDigit } == 1
 }
