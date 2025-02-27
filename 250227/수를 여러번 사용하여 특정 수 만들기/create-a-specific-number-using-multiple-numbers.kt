d2e9001e2ac32fcf7ef8d004861277262de078a5
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-27 13:21:01 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 139ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 250227/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.kt
+++ 250227/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.kt
@@ -1,43 +1,6 @@
 fun main() {
     val (a, b, c) = readln().trim().split(" ").map(String::toInt)
     val (big, small) = if (a >= b) a to b else b to a
-
-    val sums = mutableSetOf<Int>()
-
-    var addBigCount = 0
-    var sum = 0
     
-    while (sum <= c) {
-        addBigCount++
-        sum += big
-    }
-    if (sum > c) {
-        sum -= big
-        addBigCount--
-    }
 
-    sums.add(sum)
-
-    while (addBigCount > 0) {
-        sum -= big
-        addBigCount--
-        
-        var addSmallCount = 0
-        while (sum <= c) {
-            sum += small
-            addSmallCount++
-        }
-        if (sum > c) {
-            sum -= small
-            addSmallCount--
-        }
-        
-        sums.add(sum)
-
-        repeat(addSmallCount) {
-            sum -= small
-        }
-    }
-
-    println(sums.maxOf { it })
 }
