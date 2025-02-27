90981fd78e8a8e537527d8befe572dc6ce0cd9fd
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-27 11:58:18 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 865ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 250227/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.kt
@@ -0,0 +1,25 @@
+import kotlin.math.abs
+
+fun main() {
+    val numberCount = readln().toInt()
+    val numbers = readln().trim().split(" ").map(String::toInt)
+    val idxRange = (0 until numberCount)
+
+    val minDiffSum = idxRange.minOf { idx ->
+
+        val multipliedNumbers = numbers.mapIndexed { multipliedIdx, number ->
+            if (multipliedIdx == idx) number * 2
+            else number
+        }
+
+        idxRange.minOf { removedIdx ->
+            val removedNumbers = multipliedNumbers.filterIndexed { idx, number -> idx != removedIdx }    
+
+            removedNumbers.windowed(2).sumOf {
+                abs(it[0] - it[1])
+            }
+        }
+    }
+
+    println(minDiffSum)
+}
