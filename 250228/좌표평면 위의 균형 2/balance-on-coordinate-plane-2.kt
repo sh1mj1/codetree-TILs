0337bf10ee2eddcab2e4741cb254d1039e124835
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-02-28 00:41:43 +0900

    Title: 좌표평면 위의 균형 2, Time: 306ms, Memory: 27MB, Status: Passed - CodeTree

--- 250228/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.kt
+++ 250228/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.kt
@@ -12,16 +12,19 @@ fun main() {
     val minY = points.minOf { it.y }
     val maxY = points.maxOf { it.y }
 
-    val minPointsCount = (minX + 1 .. maxX - 1).minOf { x ->
+    val xRange = if (minX == maxX) listOf(minX / 2) else (minX + 1 .. maxX - 1).toList()
+    val yRange = if (minY == maxY) listOf(minY / 2) else (minY + 1 .. maxY - 1).toList()
+
+    val minPointsCount = xRange.minOf { x ->
         if (x % 2 != 0) return@minOf Int.MAX_VALUE
 
-        (minY + 1 .. maxY - 1).minOf { y ->
+        yRange.minOf { y ->
             if (y % 2 != 0) return@minOf Int.MAX_VALUE
 
             val rightUpPointsCount = points.count { it.x > x && it.y > y }
             val leftUpPointsCount = points.count { it.x < x && it.y > y }
             val leftBottomPointsCount = points.count { it.x < x && it.y < y }
-            val rightBottomPointsCount = points.count { it.x < x && it.y < y }
+            val rightBottomPointsCount = points.count { it.x > x && it.y < y }
 
             listOf(
                 rightUpPointsCount, 
