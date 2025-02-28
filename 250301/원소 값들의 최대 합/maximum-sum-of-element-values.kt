155a91c0ee668f662170cba1a4f6d63b8383ac65
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-03-01 00:00:24 +0900

    Title: 원소 값들의 최대 합, Time: 153ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 250301/원소 값들의 최대 합/maximum-sum-of-element-values.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+    val numbers = readLine()!!.split(" ").map { it.toInt() }
+
+    val maxNum = numbers.maxOrNull() ?: m
+    // 에라토스테네스의 체를 이용하여 1부터 M까지의 소수를 찾음
+    val isPrime = BooleanArray(maxNum + 1) { true }
+        isPrime[0] = false
+        isPrime[1] = false
+        for (i in 2..maxNum) {
+            if (isPrime[i]) {
+                for (j in i * 2..maxNum step i) {
+                    isPrime[j] = false
+                }
+            }
+        }
+    // 수열에서 소수만 추출하여 합 계산
+    val primeSum = numbers.filter { isPrime[it] }.sum()
+
+    println(primeSum)
+}
