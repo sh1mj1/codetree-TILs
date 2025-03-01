42d358301006cf54d8937408bb963be5e9c264b0
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-03-01 23:55:59 +0900

    Title: 원소 값들의 최대 합, Time: 138ms, Memory: 12MB, Status: Runtime Error - CodeTree

--- 250301/원소 값들의 최대 합/maximum-sum-of-element-values.kt
+++ 250301/원소 값들의 최대 합/maximum-sum-of-element-values.kt
@@ -1,21 +1,20 @@
 fun main() {
-    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
-    val numbers = readLine()!!.split(" ").map { it.toInt() }
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    val sequence = readln().split(" ").map { it.toInt() }
 
-    val maxNum = numbers.maxOrNull() ?: m
-    // 에라토스테네스의 체를 이용하여 1부터 M까지의 소수를 찾음
-    val isPrime = BooleanArray(maxNum + 1) { true }
-        isPrime[0] = false
-        isPrime[1] = false
-        for (i in 2..maxNum) {
-            if (isPrime[i]) {
-                for (j in i * 2..maxNum step i) {
-                    isPrime[j] = false
-                }
-            }
+    var maxSum = 0
+
+    for (startIndex in sequence.indices) {
+        var currentIndex = startIndex
+        var sum = 0
+
+        repeat(m) {
+            sum += sequence[currentIndex]
+            currentIndex = sequence[currentIndex] - 1
         }
-    // 수열에서 소수만 추출하여 합 계산
-    val primeSum = numbers.filter { isPrime[it] }.sum()
 
-    println(primeSum)
+        maxSum = maxOf(maxSum, sum)
+    }
+
+    println(maxSum)
 }
