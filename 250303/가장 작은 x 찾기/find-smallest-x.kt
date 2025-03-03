0f9cb7065ae74092b8b7d47bc0013d9fcbdb9002
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-03-03 22:37:42 +0900

    Title: 가장 작은 x 찾기, Time: 198ms, Memory: 15MB, Status: Passed - CodeTree

--- 250303/가장 작은 x 찾기/find-smallest-x.kt
+++ 250303/가장 작은 x 찾기/find-smallest-x.kt
@@ -1,44 +1,22 @@
-
-// 시작 x 양의 정수
-// 2 곱하기 N 번 반복
-
-// 곱할 때마다 현재 숫자 범위에 대한 단서 a, b 가 주어짐.
-// 2 배가 된 후의 값이 a 이상, b 이하임을 의미.
-
-
 fun main() {
     val n = readln().toInt()
     val ranges = List(n) {
-        readln().trim().split(" ").map(String::toInt)
-            .let{ Range(it[0], it[1]) }
+        readln().trim().split(" ").map(String::toInt).let { (start, end) -> Range(start, end) }
     }
 
-    var satisfied = false
-
-    for (i in 1 .. 5_000) {
-        var num = i
-
-        for (range in ranges) {
-            num *= 2
-            if (!range.contain(num)) break
-
-            if (range == ranges.last()) {
-                satisfied = true
-            }
-        }
-
-        if (satisfied) {
-            println(i)
-            return
-        }
+    val result = (1..5_000).firstOrNull { initial ->
+        generateSequence(initial * 2) { it * 2 }
+            .take(n)
+            .zip(ranges.asSequence())
+            .all { (num, range) -> range.contains(num) }
     }
+
+    println(result ?: "No valid number found")
 }
 
 data class Range(
     private val start: Int,
     private val end: Int,
 ) {
-    fun contain(target: Int): Boolean {
-        return target >= start && target <= end
-    }
+    fun contains(target: Int) = target in start..end
 }
