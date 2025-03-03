3a4856a9be6e1f006e49ede0fe7b0f16dd8572cc
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-03-03 22:17:56 +0900

    Title: 가장 작은 x 찾기, Time: 151ms, Memory: 15MB, Status: Passed - CodeTree

--- 250303/가장 작은 x 찾기/find-smallest-x.kt
+++ 250303/가장 작은 x 찾기/find-smallest-x.kt
@@ -1,23 +1,44 @@
+
+// 시작 x 양의 정수
+// 2 곱하기 N 번 반복
+
+// 곱할 때마다 현재 숫자 범위에 대한 단서 a, b 가 주어짐.
+// 2 배가 된 후의 값이 a 이상, b 이하임을 의미.
+
+
 fun main() {
     val n = readln().toInt()
-    val ranges = List(n) { readln().split(" ").map { it.toInt() } }
-
-    fun isReal(index: Int): Boolean {
-        var now = index
-        for (i in 0 until n) {
-            now *= 2
-            val (a, b) = ranges[i]
-            if (now !in a..b) {
-                return false
-            }
-        }
-        return true
+    val ranges = List(n) {
+        readln().trim().split(" ").map(String::toInt)
+            .let{ Range(it[0], it[1]) }
     }
 
-    for (i in 1..(ranges[0][1] / 2)) {
-        if (isReal(i)) {
-            println(i)
-            break
+    var satisfied = false
+
+    for (i in 1 .. 5_000) {
+        var num = i
+
+        for (range in ranges) {
+            num *= 2
+            if (!range.contain(num)) break
+
+            if (range == ranges.last()) {
+                satisfied = true
+            }
         }
+
+        if (satisfied) {
+            println(i)
+            return
+        }
+    }
+}
+
+data class Range(
+    private val start: Int,
+    private val end: Int,
+) {
+    fun contain(target: Int): Boolean {
+        return target >= start && target <= end
     }
 }
