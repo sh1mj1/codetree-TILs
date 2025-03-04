b7bff6215fb8d23dd1fea943cb65ae26ada19b38
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-03-04 12:37:05 +0900

    Title: 훌륭한 점프, Time: 177ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250304/훌륭한 점프/great-jump.kt
@@ -0,0 +1,31 @@
+fun main() {
+    val (n, k) = readln().trim().split(" ").map(String::toInt)
+    val stones = readln().trim().split(" ").map(String::toInt).mapIndexed { i, num ->
+        Stone(i, num)
+    }
+
+    val sortedStones = stones.sortedBy { it.num }
+
+    for (stone in sortedStones) {
+        val min = stone.num
+
+        val possibleNext = stones.filter { it.pos == 0 || it.num <= min }
+
+        if (
+            possibleNext
+                .windowed(2)
+                .all { it[1].pos - it[0].pos <= k }
+        ) {
+            println(min)
+            return
+        }
+        
+    }
+    
+
+}
+
+data class Stone(
+    val pos: Int,
+    val num: Int,
+)
