6c51fbc39265b0f7cf83618840aabe2c62686781
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-03-04 14:17:10 +0900

    Title: 훌륭한 점프, Time: 173ms, Memory: 18MB, Status: Passed - CodeTree

--- 250304/훌륭한 점프/great-jump.kt
+++ 250304/훌륭한 점프/great-jump.kt
@@ -1,33 +1,23 @@
-import java.util.*
-
 fun main() {
     val (n, k) = readln().trim().split(" ").map(String::toInt)
-    val stones = readln().trim().split(" ").map(String::toInt)
+    val stones = readln().trim().split(" ").mapIndexed { i, num -> Stone(i, num.toInt()) }
 
-    // BFS를 위한 큐
-    val queue: Queue<Pair<Int, Int>> = LinkedList()
-    queue.add(0 to stones[0]) // 시작 위치 (index, 해당 돌의 값)
+    stones
+        .sortedBy { it.num }
+        .firstOrNull { minStone ->
+            if (stones.first().num > minStone.num || stones.last().num > minStone.num) 
+                return@firstOrNull false
 
-    val visited = BooleanArray(n) { false }
-    visited[0] = true
-
-    var minMaxValue = stones[0] // 거쳐간 돌들의 값 중 최댓값의 최소값
-
-    while (queue.isNotEmpty()) {
-        val (currentIndex, currentMax) = queue.poll()
-        minMaxValue = minOf(minMaxValue, currentMax)
-
-        if (currentIndex == n - 1) {
-            println(minMaxValue)
-            return
+            val possibleNext = stones.filter { it.num <= minStone.num }
+            possibleNext
+                .windowed(2)
+                .all { (a, b) -> b.pos - a.pos <= k }
         }
-
-        for (nextIndex in currentIndex + 1 until n) {
-            if (nextIndex - currentIndex > k) break
-            if (!visited[nextIndex]) {
-                visited[nextIndex] = true
-                queue.add(nextIndex to maxOf(currentMax, stones[nextIndex]))
-            }
-        }
-    }
+        ?.num
+        ?.let(::println)
 }
+
+data class Stone(
+    val pos: Int,
+    val num: Int,
+)
