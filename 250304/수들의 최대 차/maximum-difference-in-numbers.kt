06f174b33a4fa63345210a16ae142c8e24cda592
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-03-04 11:34:14 +0900

    Title: 수들의 최대 차, Time: 307ms, Memory: 19MB, Status: Passed - CodeTree

--- 250304/수들의 최대 차/maximum-difference-in-numbers.kt
+++ 250304/수들의 최대 차/maximum-difference-in-numbers.kt
@@ -4,27 +4,18 @@ fun main() {
     val (n, k) = readln().trim().split(" ").map(String::toInt)
     val numbers = List(n) { readln().toInt() }
     
-    val ascendingNumbers = numbers.sorted()
-    val descendingNumbers = numbers.sortedDescending()
+    val sortedNumbers = numbers.sorted()
 
-    for (i in n downTo 0) {
-        val ascending = ascendingNumbers.subList(0, i)
-        
-        val ascendingMax = ascending.maxOf { it }
-        val ascendingMin = ascending.minOf { it }
-        if (abs(ascendingMax - ascendingMin) <= k) {
-            println(i)
-            return
-        }
-
-        val descending = descendingNumbers.subList(0, i)
-
-        val descendingMax = descending.maxOf { it }
-        val descendingMin = descending.minOf { it }
-
-        if (abs(descendingMax - descendingMin) <= k) {
-            println(i)
-            return
+    val maxCount = sortedNumbers.withIndex().maxOf { (i, minNum) ->
+        (n - 1 downTo i).maxOf { j ->
+            val maxNum = sortedNumbers[j]
+            if (maxNum - minNum <= k) {
+                j - i + 1 // count
+            } else {
+                0
+            }
         }
     }
+
+    println(maxCount)
 }
