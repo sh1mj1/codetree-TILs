93e60157b9c951c0a1f5848e2e4ba90d3bcc0ab2
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-04-07 16:54:33 +0900

    Title: 사다리 타기, Time: 291ms, Memory: 18MB, Status: Passed - Codetree

--- 250407/사다리 타기/ladder-game.kt
+++ 250407/사다리 타기/ladder-game.kt
@@ -5,65 +5,68 @@ fun main() {
         val (a, b) = readln().split(" ").map { it.toInt() }
         lines.add(Pair(b, a - 1))
     }
-    // Please write your code here.
-    // line 은 0 부터 n-1 까지
-    val horizontalLines = lines.map { line ->
-        HorizontalLine(
-            p1 = line.second,
-            p2 = line.second + 1,
-            height = line.first
-        )
-    }
-    .sortedBy { it.height }
 
-    val maxHeight = horizontalLines.maxOf { it.height }
+    val board = Array(16) { IntArray(12) }
+    var answer = 100
+    val linesToRemove = mutableListOf<Pair<Int, Int>>()
+    var originResult = listOf<Int>()
 
+    fun makeBoard() {
+        for (i in board.indices) board[i].fill(0)
 
-    fun runStep(line: Int, height: Int): Int {
-        if (height > maxHeight) return line
-
-        val sameHeightLines = horizontalLines.filter { it.height == height }
-
-        val meetingLine = sameHeightLines.find {
-            it.p1 == line || it.p2 == line
+        for ((x, y) in lines) {
+            board[x][y] = 1
+            board[x][y + 1] = 2
         }
 
-        if (meetingLine == null) return runStep(line, height + 1)
-
-        if (meetingLine.p1 == line) return runStep(meetingLine.p1, height + 1)
-
-        return runStep(meetingLine.p2, height + 1)
+        for ((x, y) in linesToRemove) {
+            board[x][y] = 0
+            board[x][y + 1] = 0
+        }
     }
 
-    val results = (0 until n).map { line ->
-        Result(
-            start = line,
-            dest = runStep(line, 1)
-        )
+    fun play(): List<Int> {
+        makeBoard()
+        val result = MutableList(n) { 0 }
+
+        for (player in 0 until n) {
+            var x = 0
+            var y = player
+
+            while (x < 16) {
+                if (board[x][y] == 1 && y + 1 < n && board[x][y + 1] == 2) {
+                    y++
+                } else if (board[x][y] == 2 && y - 1 >= 0 && board[x][y - 1] == 1) {
+                    y--
+                }
+                x++
+            }
+            result[y] = player
+        }
+        return result
     }
-    .also(::println)
-    
 
-    
+    fun isPossible(result: List<Int>): Boolean {
+        return result == originResult
+    }
 
+    fun findMinLines(idx: Int) {
+        if (idx == m) {
+            val result = play()
+            if (isPossible(result)) {
+                val lineCount = m - linesToRemove.size
+                answer = minOf(answer, lineCount)
+            }
+            return
+        }
+        linesToRemove.add(lines[idx])
+        findMinLines(idx + 1)
 
+        linesToRemove.removeLastOrNull()
+        findMinLines(idx + 1)
+    }
+
+    originResult = play()
+    findMinLines(0)
+    println(answer)
 }
-
-
-
-data class HorizontalLine(
-    val p1: Int,
-    val p2: Int,
-    val height: Int,
-)
-
-data class Result(
-    val start: Int,
-    val dest: Int,
-)
-
-
-/*
-
-
-*/
