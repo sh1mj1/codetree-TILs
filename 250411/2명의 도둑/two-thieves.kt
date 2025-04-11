ff1b63da28150e3f5e569f3903d8667ccc38444a
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-04-11 16:44:41 +0900

    Title: 2명의 도둑, Time: 137ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250411/2명의 도둑/two-thieves.kt
+++ 250411/2명의 도둑/two-thieves.kt
@@ -1,59 +1,81 @@
 import java.util.*
 
-fun main() {
-    val scanner = Scanner(System.`in`)
-    val n = scanner.nextInt()
-    val m = scanner.nextInt()
-    val c = scanner.nextInt()
+const val MAX_M = 5
+const val MAX_N = 10
 
-    val grid = Array(n) { IntArray(n) }
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            grid[i][j] = scanner.nextInt()
+var n = 0
+var m = 0
+var c = 0
+
+val weight = Array(MAX_N) { IntArray(MAX_N) }
+val a = IntArray(MAX_M)
+
+var ans = 0
+var maxVal = 0
+
+fun findMaxSum(currIdx: Int, currWeight: Int, currVal: Int) {
+    if (currIdx == m) {
+        if (currWeight <= c) {
+            maxVal = maxOf(maxVal, currVal)
         }
+        return
     }
 
-    var answer = 0
-    fun calculateMaxSum(index: Int): Int {
-        val row = index / n
-        val col = index % n
-        val arr = IntArray(m) { grid[row][col + it] }
+    // 현재 인덱스 선택 안 함
+    findMaxSum(currIdx + 1, currWeight, currVal)
 
-        var max = 0
-        for (bit in 0 until (1 shl m)) {
-            val subset = mutableListOf<Int>()
-            for (i in 0 until m) { 
-                if (bit and (1 shl i) != 0) {
-                    subset.add(arr[i])
+    // 현재 인덱스 선택
+    val w = a[currIdx]
+    findMaxSum(currIdx + 1, currWeight + w, currVal + w * w)
+}
+
+fun findMax(sx: Int, sy: Int): Int {
+    for (i in 0 until m) {
+        a[i] = weight[sx][sy + i]
+    }
+
+    maxVal = 0
+    findMaxSum(0, 0, 0)
+    return maxVal
+}
+
+fun intersect(a: Int, b: Int, c: Int, d: Int): Boolean {
+    return !(b < c || d < a)
+}
+
+fun possible(sx1: Int, sy1: Int, sx2: Int, sy2: Int): Boolean {
+    if (sy1 + m - 1 >= n || sy2 + m - 1 >= n)
+        return false
+
+    if (sx1 != sx2)
+        return true
+
+    return !intersect(sy1, sy1 + m - 1, sy2, sy2 + m - 1).not()
+}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    n = sc.nextInt()
+    m = sc.nextInt()
+    c = sc.nextInt()
+
+    for (i in 0 until n)
+        for (j in 0 until n)
+            weight[i][j] = sc.nextInt()
+
+    for (sx1 in 0 until n) {
+        for (sy1 in 0 until n) {
+            for (sx2 in 0 until n) {
+                for (sy2 in 0 until n) {
+                    if (possible(sx1, sy1, sx2, sy2)) {
+                        val val1 = findMax(sx1, sy1)
+                        val val2 = findMax(sx2, sy2)
+                        ans = maxOf(ans, val1 + val2)
+                    }
                 }
             }
-            var sum = 0
-            var value = 0
-            for (num in subset) {
-                sum += num
-                value += num * num
-            }
-            if (sum <= c) {
-                max = max.coerceAtLeast(value)
-            }
-        }
-        return max
-    }
-
-    fun combination(count: Int, start: Int, selected: IntArray) {
-        if (count == 2) {
-            val sum = calculateMaxSum(selected[0]) + calculateMaxSum(selected[1])
-            answer = maxOf(answer, sum)
-            return
-        }
-
-        for (i in start until n * n) {
-            if (i % n + m > n) continue
-            selected[count] = i
-            combination(count + 1, i + m, selected)
         }
     }
 
-    combination(0, 0, IntArray(2))
-    println(answer)
+    println(ans)
 }
