96553cae63de3a01041921d69b592edd8d9f357f
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-04-11 15:03:46 +0900

    Title: 2명의 도둑, Time: 150ms, Memory: 13MB, Status: Wrong Answer - Codetree

--- 250411/2명의 도둑/two-thieves.kt
+++ 250411/2명의 도둑/two-thieves.kt
@@ -1,58 +1,90 @@
+package poke.rogue.helper.presentation.dex.detail
+
+
 import kotlin.math.max
 
 fun main() {
-    // n: 격자 크기     m: 연속 열 선택 개수    c: 최대 무게
     val (n, m, c) = readln().split(" ").map { it.toInt() }
     val weight = Array(n) { readln().split(" ").map { it.toInt() } }
     // Please write your code here.
-    var maxTotalSum = 0
+    var totalMaxPriceSum = 0
 
-    val visited = List(n) { BooleanArray(n) { false } }
+    fun thief2(
+        startIdx: Int,
+        currentCombination: List<Int>,
+        cWeight: List<Int>,
+        thief1Sum: Int,
+    ) {
+        for (idx in startIdx until n) {
+            val cComb = currentCombination + cWeight[idx]
 
-    fun isInRange(idx: Int): Boolean {
-        return idx in 0 until n
-    }
+            val currentWeightSum =  cComb.sumOf { it }
+            if (currentWeightSum > c) return
 
-    fun t2Sum(row: Int): Int {
-        val currentRow = weight[row]
-        var s = 0
-        var e = 0
+            val priceSum = cComb.sumOf { it * it }
+            totalMaxPriceSum = max(totalMaxPriceSum, thief1Sum + priceSum)
 
-        var maxPrice = 0
-
-        while(s < n) {
-            if (!isInRange(s)) break
-
-            if (visited[row][s]) {
-                s++
-                e = s
-                continue
-            }
-            
-            val weightSum = (s .. e).sumOf { currentRow[it] }
-            if (weightSum > c) {
-                s++
-                e = s
-                continue
-            }
-
-            val priceSum = (s .. e).sumOf { currentRow[it] * currentRow[it] }
-            maxPrice = max(priceSum, maxPrice)
-
-            e++
-
-            if (!isInRange(e) || visited[row][e]) {
-                s++
-                e = s
-                continue
-            }
-
+            return thief2(
+                startIdx = idx + 1,
+                currentCombination = cComb,
+                cWeight = cWeight,
+                thief1Sum = thief1Sum,
+            )
         }
-        return maxPrice
     }
 
-    println(t2Sum(0))
-    println(t2Sum(0))
-    println(t2Sum(0))
+    fun thief1(
+        startIdx: Int,
+        row: Int,
+        currentCombination: List<Int>,
+    ) {
+        for (idx in startIdx until n) {
+            var cComb = currentCombination + weight[row][idx]
+            val currentWeightSum = cComb.sumOf { it }
+            if (currentWeightSum > c) return
 
+            val priceSum = cComb.sumOf { it * it }
+            
+            for (thief2Row in 0 until n) {
+                if (thief2Row == row) {
+                    for (startIdx in idx + 1 until n) {
+                        thief2(
+                            startIdx = startIdx,
+                            currentCombination = emptyList(),
+                            cWeight = weight[thief2Row],
+                            thief1Sum = priceSum,
+                        )
+                    }
+                } else {
+                    for (startIdx in 0 until n) {
+                        thief2(
+                            startIdx = startIdx,
+                            currentCombination = emptyList(),
+                            cWeight = weight[thief2Row],
+                            thief1Sum = priceSum,
+                        )
+                    }
+                }
+            }
+            return thief1(
+                startIdx = idx + 1,
+                row = row,
+                currentCombination = cComb,
+            )
+        }
+    }
+
+    for (row in 0 until n) {
+        for (sIdx in 0 until n) {
+            thief1(
+                startIdx = sIdx,
+                row = row,
+                currentCombination = emptyList(),
+            )
+        }
+    }
+
+    println(
+        totalMaxPriceSum
+    )
 }
