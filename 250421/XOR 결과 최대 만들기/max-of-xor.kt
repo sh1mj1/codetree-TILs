b649d4947eed009ad5c5165d09eee896970afbb9
sh1mj1 <90040304+sh1mj1@users.noreply.github.com>
2025-04-21 18:24:27 +0900

    Title: XOR 결과 최대 만들기, Time: 231ms, Memory: 24MB, Status: Passed - Codetree

--- 250421/XOR 결과 최대 만들기/max-of-xor.kt
+++ 250421/XOR 결과 최대 만들기/max-of-xor.kt
@@ -1,26 +1,32 @@
+import kotlin.math.max
+
 fun main() {
     val (n, m) = readln().trim().split(" ").map { it.toInt() }
     val numbers = readln().trim().split(" ").map { it.toInt() }
     // Please write your code here.
 
-    fun combinations(n: Int, m: Int): List<List<Int>> {
-        fun helper(idx: Int, remain: Int): List<List<Int>> {
-            if (remain == 0) return listOf(emptyList())
-
-            return (idx until n).flatMap { i ->
-                helper(i + 1, remain - 1).map { it + numbers[i] }
-            }
-        }
-        return helper(0, m)
-    }
-
-    val combinations = combinations(n, m)
-
-    val answer = combinations.maxOf { combi ->
-        combi.reduce { acc, number -> 
+    fun xorResult(combi: List<Int>): Int {
+        return combi.reduce { acc, number -> 
             acc xor number
         }
     }
 
-    println(answer)
+    fun answer(n: Int, m: Int): Int {
+        var answer: Int = Int.MIN_VALUE
+        fun find(numIdx: Int, remain: Int, curCombi: List<Int>) {
+            if (remain == 0) {
+                answer = max(answer, xorResult(curCombi))
+                return
+            }
+
+            (numIdx until n).forEach {
+                find(it + 1, remain - 1, curCombi + numbers[it])
+            }
+        }
+
+        find(0, m, emptyList())
+        return answer
+    }
+
+    println(answer(n, m))
 }
